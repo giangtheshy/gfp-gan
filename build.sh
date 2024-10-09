199138513f53bf67462103adec6252e5c4d3ae48
giangtheshy <nhockrong98@gmail.com>
2024-10-09 20:01:56 +0700

    fix: update import statement for rgb_to_grayscale in degradation script

--- build.sh
+++ build.sh
@@ -15,4 +15,4 @@ mkdir -p experiments/pretrained_models/
 
 wget https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth -P experiments/pretrained_models/
 
-# sed -i 's/from torchvision.transforms.functional_tensor import rgb_to_grayscale/from torchvision.transforms.functional import rgb_to_grayscale/' venv/lib/python3.8/site-packages/basicsr/data/degradations.py
+sed -i 's/from torchvision.transforms.functional_tensor import rgb_to_grayscale/from torchvision.transforms.functional import rgb_to_grayscale/' venv/lib/python3.8/site-packages/basicsr/data/degradations.py
